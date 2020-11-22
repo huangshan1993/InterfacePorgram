bc906ffced8be361483df8eca48f6b34924ced67
huangshan <1372605810@qq.com>
2020-11-22 23:31:58 +0800

    delete ./Login

--- Login/View/__init__.py
+++
@@ -1,8 +0,0 @@
-#-*-coding:utf-8-*-#
-#--------------------------------------------
-#@ProjectName: Study 
-#@FileName:__init__.py
-#@Author: huangshan
-#@Date: 2020/7/11 16:22
-#@Description:
-#-----------------------------------------------
